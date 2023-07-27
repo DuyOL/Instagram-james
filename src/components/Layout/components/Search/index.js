f0c1caabe3fb0cd02951b9d7d7d63ab11b1d3047
DuyOL <duyskt228@gmail.com>
2023-07-27 08:51:13 +0700

    27-07

--- src/components/Layout/components/Search/index.js
+++ src/components/Layout/components/Search/index.js
@@ -9,22 +9,31 @@ import classNames from 'classnames/bind';
 import { wrapper as PopperWrapper } from '~/components/Popper';
 import AccountItem from '~/components/AccountItem';
 import { SearchIcon } from '~/components/Icons';
-import styles  from './Search.module.scss';
-import { faLess } from '@fortawesome/free-brands-svg-icons';
+import styles from './Search.module.scss';
+
 const cx = classNames.bind(styles)
 
 function Search() {
     const [searchValue, setSearchValue] = useState('')
     const [searchResult, setSearchResult] = useState([]);
     const [showResult, setShowResult] =useState(true);
+    const [loading, setLoading] =useState(false);
 
     const inputRef = useRef()
 
     useEffect(() => {
-        setTimeout(() => {
-            setSearchResult([1 , 2 ,3])
-        }, 0)
-    }, []);
+        if (!searchValue.trim()) {
+            setSearchResult([]);
+            return
+        }
+        setLoading(true);
+        fetch(`https://tiktok.fullstack.edu.vn/api/users/search?q=${encodeURIComponent(searchValue)}&type=less`)
+            .then((res) => res.json())
+            .then((res) => {
+                setSearchResult(res.data);
+                setLoading(false);
+            });
+    }, [searchValue]);
 
     const handClear = () => {                
         setSearchValue('');
@@ -45,11 +54,10 @@ function Search() {
                                 <h4 className={cx('search-title')}>
                                      Accounts
                                 </h4>
-                                     <AccountItem />
-                                     <AccountItem />
-                                     <AccountItem />
-                                     <AccountItem />
-                                     <AccountItem />
+                                {searchResult.map((result) => {
+                                    return <AccountItem key={result.id} data={result} />;
+                                })}
+
                                 </PopperWrapper>
                             </div>                          
             )}
@@ -64,13 +72,13 @@ function Search() {
                     onChange={e => setSearchValue(e.target.value)}
                     onFocus={() => setShowResult(true)}
                 />
-                {!!searchValue && (
+                {!!searchValue && !loading && (
                     <button className={cx('clear')}
                         onClick={handClear}>
                         <FontAwesomeIcon icon={faCircleXmark} />
                     </button>
                 )}
-                         {/* <FontAwesomeIcon className={cx('loading')} icon={faSpinner} /> */}
+                         {loading && <FontAwesomeIcon className={cx('loading')} icon={faSpinner} />} 
                     
                         <button className={cx('search-btn')}>
                             <SearchIcon />
