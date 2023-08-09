64362b72e497876de15f8ccad9af7507f30585ef
DuyOL <duyskt228@gmail.com>
2023-08-09 19:49:03 +0700

    Chiều 09-08

--- src/pages/Real/Real.js
+++ src/pages/Real/Real.js
@@ -1,14 +1,71 @@
+import React, { useState, useRef, useEffect } from 'react';
 import styles from './Real.module.scss';
 import classNames from 'classnames/bind';
+import videoData from './videoData';
 
 const cx = classNames.bind(styles);
 
 function Real() {
-        return (
+    const videoRefs = useRef([]);
+    const [activeIndex, setActiveIndex] = useState(null);
+    // Thêm null để không có video nào được chạy ban đầu
+
+    const handleVideoClick = (index) => {
+        // Tắt và đặt thời gian về 0 cho tất cả video
+        videoRefs.current.forEach((videoRef) => {
+            videoRef.pause();
+            videoRef.currentTime = 0;
+            videoRef.muted = true;
+        });
+
+        // Bật âm thanh và chạy video mới
+        videoRefs.current[index].play();
+        videoRefs.current[index].muted = false;
+        setActiveIndex(index);
+    };
+
+    useEffect(() => {
+        const handleScroll = () => {
+            const scrollPosition = window.scrollY;
+
+            // Tắt âm thanh cho tất cả video
+            videoRefs.current.forEach((videoRef) => {
+                videoRef.muted = true;
+            });
+
+            // Bật âm thanh cho video đang hoạt động
+            if (activeIndex !== null) {
+                videoRefs.current[activeIndex].muted = false;
+            }
+        };
+
+        window.addEventListener('scroll', handleScroll);
+
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+    }, [activeIndex]);
+
+    return (
         <div className={cx('wrapper')}>
-           <h1> Thước Phim </h1>
+            {videoData.map((video, index) => (
+                <div
+                    key={index}
+                    className={cx('video-container', { active: index === activeIndex })}
+                    onClick={() => handleVideoClick(index)}
+                >
+                    <video
+                        ref={(ref) => (videoRefs.current[index] = ref)}
+                        controls
+                        className={cx('video')}
+                    >
+                        <source src={video.src} type="video/mp4" />
+                    </video>
+                    <p className={cx('video-title')}>{video.title}</p>
+                </div>
+            ))}
         </div>
-    )
+    );
 }
 
 export default Real;
---
+++ src/pages/Real/videoData.js
@@ -0,0 +1,21 @@
+import video1 from '~/pages/Home/Videos/video1.mp4';
+import video2 from '~/pages/Home/Videos/video2.mp4';
+import video3 from '~/pages/Home/Videos/video3.mp4';
+import video4 from '~/pages/Home/Videos/video4.mp4';
+import video5 from '~/pages/Home/Videos/video5.mp4';
+import video6 from '~/pages/Home/Videos/video6.mp4';
+import video7 from '~/pages/Home/Videos/video7.mp4';
+import video8 from '~/pages/Home/Videos/video8.mp4';
+
+const videoData = [
+    { src: video1, title: 'Video 1' },
+    { src: video2, title: 'Video 2' },
+    { src: video3, title: 'Video 3' },
+    { src: video4, title: 'Video 4' },
+    { src: video5, title: 'Video 5' },
+    { src: video6, title: 'Video 6' },
+    { src: video7, title: 'Video 7' },
+    { src: video8, title: 'Video 8' },
+];
+
+export default videoData;
